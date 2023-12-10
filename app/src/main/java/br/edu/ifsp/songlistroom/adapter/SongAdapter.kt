b36eb5917c41f355b9e36fa97d69cf5e5f5541fa
gabrielmoreira-dev <g.alves.m.2008@gmail.com>
2023-12-09 22:12:35 -0300

    User Story #6: Add search

--- app/src/main/java/br/edu/ifsp/songlistroom/adapter/SongAdapter.kt
+++ app/src/main/java/br/edu/ifsp/songlistroom/adapter/SongAdapter.kt
@@ -3,18 +3,22 @@ package br.edu.ifsp.songlistroom.adapter
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import br.edu.ifsp.songlistroom.data.Song
 import br.edu.ifsp.songlistroom.databinding.SongCellBinding
 
-class SongAdapter(): RecyclerView.Adapter<SongAdapter.SongViewHolder>() {
+class SongAdapter(): RecyclerView.Adapter<SongAdapter.SongViewHolder>(), Filterable {
     private lateinit var binding: SongCellBinding
     private var listener: SongListener? = null
     var songList = ArrayList<Song>()
+    var songListFiltered = ArrayList<Song>()
 
     @SuppressLint("NotifyDataSetChanged")
     fun updateList(newList: ArrayList<Song>) {
         songList = newList
+        songListFiltered = songList
         notifyDataSetChanged()
     }
 
@@ -28,13 +32,33 @@ class SongAdapter(): RecyclerView.Adapter<SongAdapter.SongViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
-        songList[position].let {
+        songListFiltered[position].let {
             holder.name.text = it.name
             holder.artist.text = it.artist
         }
     }
 
-    override fun getItemCount() = songList.size
+    override fun getItemCount() = songListFiltered.size
+
+    override fun getFilter() = object : Filter() {
+        override fun performFiltering(p0: CharSequence?): FilterResults {
+            songListFiltered = if (p0.toString().isEmpty()) songList else {
+                ArrayList(songList.filterIndexed { _, song ->
+                    song.name.lowercase().contains(p0.toString())
+                })
+            }
+            FilterResults().let {
+                it.values = songListFiltered
+                return it
+            }
+        }
+
+        @SuppressLint("NotifyDataSetChanged")
+        override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+            songListFiltered = p1?.values as ArrayList<Song>
+            notifyDataSetChanged()
+        }
+    }
 
     inner class SongViewHolder(view: SongCellBinding): RecyclerView.ViewHolder(view.root) {
         val name = view.nameTv
--- app/src/main/java/br/edu/ifsp/songlistroom/ui/SongListFragment.kt
+++ app/src/main/java/br/edu/ifsp/songlistroom/ui/SongListFragment.kt
@@ -2,9 +2,16 @@ package br.edu.ifsp.songlistroom.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -37,6 +44,27 @@ class SongListFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.main_menu, menu)
+                val searchView = menu.findItem(R.id.actionSearch).actionView as SearchView
+                searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(p0: String?) = false
+
+                    override fun onQueryTextChange(p0: String?): Boolean {
+                        songAdapter.filter.filter(p0)
+                        return true
+                    }
+                })
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem) = false
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
     private fun configureRecyclerView() {
         songAdapter = SongAdapter()
         viewModel.songList.observe(viewLifecycleOwner) {
@@ -51,7 +79,7 @@ class SongListFragment : Fragment() {
 
     private fun setOnItemClickedListener() = object : SongAdapter.SongListener {
         override fun onItemClicked(pos: Int) = Bundle().let {
-            it.putInt("songId", songAdapter.songList[pos].id)
+            it.putInt("songId", songAdapter.songListFiltered[pos].id)
             findNavController().navigate(R.id.action_songListFragment_to_songDetailFragment, it)
         }
     }.let {
